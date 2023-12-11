a5333d8c0e9593dc5112e26a24058899c1b62736
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-12-11 23:07:19 +0200

    range tree basic ini

---
+++ range_tree/range_tree.py
@@ -0,0 +1,37 @@
+"""
+Η λογική ενός τρισδιάστατου (3D) Range Tree είναι ότι για μία δοθείσα συλλογή από σημεία P,
+κατασκευάζουμε αρχικά ένα BBST (Binary Balanced Search Tree) βασισμένο στις συντεταγμένες x
+των σημείων. Έπειτα, κάθε κόμβος του αρχικού BBST, αποθηκεύει ένα δευτερεύον BBST, το οποίο
+κατασκευάζεται από τα σημεία που έχουν την ίδια συντεταγμένη x με τον τρέχοντα κόμβο, αλλά
+ταξινομημένα βάσει των συντεταγμένων y τους. Αντίστοιχα για τα σημεία z.
+"""
+
+
+class Node1D:
+    def __init__(self):
+        pass
+
+
+class RangeTree1D:
+    def __init__(self):
+        pass
+
+
+class Node2D:
+    def __init__(self):
+        pass
+
+
+class RangeTree2D:
+    def __init__(self):
+        pass
+
+
+class Node3D:
+    def __init__(self):
+        pass
+
+
+class RangeTree3D:
+    def __init__(self):
+        pass
