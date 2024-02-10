8cc4217c5e1b66b6e17e7890ebd05d53d9bb4329
Theodore Giannak <59723723+TheodoreGiannak@users.noreply.github.com>
2024-02-10 21:04:31 +0200

    Corrections (to be continued)

--- r_tree/r_tree.py
+++ r_tree/r_tree.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from rtree import index
-from auxiliary import letter_normalization
 
 class RTree:
     def __init__(tree):
@@ -14,35 +13,36 @@ class RTree:
     def search(tree, qbbox):
         return list(tree.idx.intersection(qbbox))
 
+def letter_normalization(letter):
+    return ord(letter.upper())
 
-def build_rtree():
-    df = pd.read_csv("./scientists_data.csv")
+def create_rtree(file_csv):
+    df = pd.read_csv(file_csv)
     rtree = RTree()  # Δημιουργία ενός νέου R-tree
 
-    # Για κάθε εγγραφή του dataframe, υπολογίζουμε τις συντεταγμένες (x, y,z) βάσει της αριθμητική τιμής του πρώτου γράμματος του επωνύμου του αριθμού των βραβείων και του αριθμού των σημοσιεύεσεων στο DBLP Record αντίστοιχα, και εισάγουμε το στοιχείο στο R-tree.
+    # Για κάθε εγγραφή του dataframe, υπολογίζουμε τις συντεταγμένες (x, y, z) βάσει της αριθμητική τιμής του πρώτου γράμματος του επωνύμου, του αριθμού των βραβείων και του αριθμού των δημοσιεύεσεων στο DBLP Record αντίστοιχα, και εισάγουμε το στοιχείο στο R-tree.
     for i in range(len(df)):
         x = letter_normalization(df.iloc[i]['surname'][0])
         y = df.iloc[i]['awards']
-        z = df.iloc[i]['BDLP_Record']
-        data = (df.iloc[i]['surname'], df.iloc[i]['awards'], df.iloc[i]['BDLP_Record'])
+        z = df.iloc[i]['DBLP_Record']
+        data = (df.iloc[i]['surname'], df.iloc[i]['awards'], df.iloc[i]['DBLP_Record'])
         rtree.insert(i, data, x, y, z)  # Εισαγωγή του στοιχείου στο R-tree
 
     return rtree
 
 
-def query_rtree(rtree, minLetter, maxLetter, numAwards, minDLP_Record, maxDLP_Record):
+def query_rtree(rtree, minLetter, maxLetter, minAwards, maxAwards, minDLP_Record, maxDLP_Record):
     # Υπολογισμός των αριθμητικών τιμών του ελάχιστου και του μέγιστου γράμματος
-
     minLetter = letter_normalization(minLetter)
     maxLetter = letter_normalization(maxLetter)
 
-    qbbox = (minLetter, minDLP_Record, numAwards, maxDLP_Record, maxLetter, float('inf'))
+    qbbox = (minLetter, minDLP_Record, minAwards, maxAwards, maxDLP_Record, maxLetter, float('inf'))
     matches = rtree.search(qbbox)  # Αναζήτηση στο R-tree με βάση το δοθέν query_bbox
 
     queryResults = []
     # Ανάκτηση των δεδομένων από τα αποτελέσματα της αναζήτησης
     for match in matches:
-        surname, awards, BDLP_Record = rtree.data_list[match]
-        queryResults.append({"surname": surname, "awards": awards, "BDLP_Record": BDLP_Record})
+        surname, awards, education, DBLP_Record, = rtree.dataList[match]
+        queryResults.append({"surname": surname, "awards": awards, "education": education,"DBLP_Record": DBLP_Record})
 
     return queryResults
