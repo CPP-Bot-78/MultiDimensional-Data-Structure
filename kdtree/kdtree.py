4e3aaeb9928319ef14c7350da5dc9ca86389aebe
PanosStath <126835414+PanosStath@users.noreply.github.com>
2024-02-12 01:29:13 +0200

    Update kdtree.py

--- kdtree/kdtree.py
+++ kdtree/kdtree.py
@@ -7,7 +7,7 @@ class KDNode:
         self.right = right
 
 class KdTree:
-    def __init__(self, points, depth=0):
+    def __init__(self, points, depth=0, min_awards=0):
         if not points:
             self.root = None
         else:
@@ -16,16 +16,21 @@ class KdTree:
             points.sort(key=lambda x: x[1][axis] if isinstance(x[1], (list, tuple)) else x[1])
             median = len(points) // 2
             self.root = KDNode(points[median])
-            self.root.left = KdTree(points[:median], depth + 1).root
-            self.root.right = KdTree(points[median + 1:], depth + 1).root
+            self.root.left = KdTree(points[:median], depth + 1, min_awards).root
+            self.root.right = KdTree(points[median + 1:], depth + 1, min_awards).root
+            self.min_awards = min_awards
 
-    def range_query(self, node, results, surname_range, awards_range, dblp_range):
+    def range_query(self, node, results, surname_range, dblp_range):
         if node is not None:
-            if surname_range[0] <= node.data[0][0].upper() <= surname_range[1] and awards_range[0] <= node.data[1][0] <= awards_range[1] and dblp_range[0] <= int(node.data[2][0]) <= dblp_range[1]:
-                results.append(node.data)
+            if (
+                surname_range[0] <= node.data[0][0].upper() <= surname_range[1]
+                and self.min_awards <= node.data[1][0]
+                and dblp_range[0] <= int(node.data[2][0]) <= dblp_range[1]
+            ):
+              	results.append(node.data)
 
-            self.range_query(node.left, results, surname_range, awards_range, dblp_range)
-            self.range_query(node.right, results, surname_range, awards_range, dblp_range)
+            self.range_query(node.left, results, surname_range, dblp_range)
+            self.range_query(node.right, results, surname_range, dblp_range)
 
 def load_scientist_data():
     data = pd.read_csv('computer_scientists_data.csv')
@@ -39,3 +44,4 @@ def load_scientist_data():
 
 def convert_to_list(value):
     return [int(val) for val in str(value)[1:-1].split(', ')] if pd.notna(value) and str(value).startswith('[') else [int(value)]
+    
