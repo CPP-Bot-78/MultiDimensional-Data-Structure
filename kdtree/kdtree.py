aed399d86cacfa16b630ef00f9efb447112d056a
CPP-Bot-78 <kzachoulitis@gmail.com>
2024-02-15 14:59:10 +0200

    fixed query_kdtree

--- kdtree/kdtree.py
+++ kdtree/kdtree.py
@@ -52,13 +52,18 @@ def build_kdtree(min_awards: int):
     return kdtree
 
 
-def query_kdtree(kdtree, results, surname_range, dblp_range):
-    results = kdtree.range_query(
+def query_kdtree(kdtree, surname_1, surname_2, awards, min_dblp, max_dblp):
+    results = []
+    surname_range = [surname_1, surname_2]
+    dblp_range = [min_dblp, max_dblp]
+    kdtree.range_query(
         kdtree.root,
+        results,
         surname_range,
         dblp_range
     )
     return results
+
 
 def load_scientist_data():
     try:
