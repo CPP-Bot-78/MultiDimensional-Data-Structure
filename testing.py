2e0f3e64f2126903b57d6e9fdf627050998b02f4
CPP-Bot-78 <kzachoulitis@gmail.com>
2024-02-16 11:32:45 +0200

    requirements & testing comments

--- testing.py
+++ testing.py
@@ -8,13 +8,22 @@ from kdtree.kdtree import build_kdtree, query_kdtree
 from r_tree.r_tree import create_rtree, query_rtree_by_range
 from demo import create_new_demo
 
-TREES = []
+# Οι συναρτήσεις δημιουργίας και αναζήτησης των δέντρων.
+# Είναι ευθυγραμμισμένες ώστε με το ίδιο index να παίρνουμε το ίδιο αποτέλεσμα
 BUILD_FUNCS = [create_rtree, build_octree, build_kdtree, build_range_tree]
 QUERY_FUNCS = [query_rtree_by_range, query_octree, query_kdtree, query_range_tree_by_ranges]
-RESULTS = []
 
 
 def save_experiment(trees: list, results: list, test: list):
+    """
+    Σώζει τα αποτελέσματα του test στον φάκελο testing
+    :param list trees: Η λίστα με τα δέντρα που ελέγχθηκαν. Περιέχει αντικείμενα
+    :param list results: Λίστα που περιέχει τα αποτελέσματα(λίστες) από κάθε δέντρο
+    :param list test: To Test Set ώστε να ξέρουμε πως προέκυψαν τα αποτελέσματα.
+    :return: Nothing
+    :rtype: None
+    """
+    # Το dir που δημιουργηθεί ο φάκελος αν δεν υπάρχει ήδη
     script_directory = os.path.dirname(os.path.abspath(__file__))
     FOLDERNAME = 'testing'
     FOLDERNAME = os.path.join(script_directory, FOLDERNAME)
@@ -23,13 +32,19 @@ def save_experiment(trees: list, results: list, test: list):
         os.makedirs(folder_path)
     os.chdir(FOLDERNAME)
     if os.path.exists('test.txt'):
+        # Δημιουργεί ένα αρχείο με το ίδιο όνομα αλλά με ένα αριθμό πχ test2.
+        # Όσο υπάρχουν αρχεία με το ίδιο όνομα θα δοκιμάζει με αυξημένο κατά 1 count, μέχρι να φτάσει στο 50
         demo_name = create_new_demo('test.txt', 1)
     else:
+        # Δεν υπάρχει ήδη άρα θα μείνει ως έχει
         demo_name = 'test.txt'
+    # Δημιουργούμε το αρχείο. Δημιουργείται εδώ ώστε στο for loop μετά να γίνεται append,
+    # ώστε να κάθε αρχείο να έχει και τα 4 δέντρα
     with open(demo_name, 'w') as f:
         f.write("")
-
+    # Επαναλαμβάνουμε για κάθε δέντρο
     for tree in trees:
+        # Αντιστοίχηση του αποτελέσματος με index του αντικειμένου.
         tree_results = results[trees.index(tree)]
         with open(demo_name, 'a', encoding='utf-8') as file:
             file.write(f"Results for {tree.__str__()}:\n")
@@ -47,6 +62,13 @@ def save_experiment(trees: list, results: list, test: list):
 
 
 def get_test_set():
+    """
+    Ετοιμάζει ενα τυχαίο set για τη δοκιμή των δέντρων.
+    :return: Λίστα που περιέχει [λίστα με 2 τυχαίους χαρακτήρες με αλφαβητική σειρά], τυχαίο ακέραιο για τα βραβεία,
+    [λίστα με 2 ακεραίους για το DBLP από τον μικρότερο στον μεγαλύτερο], τυχαίο float για το lsh μεταξύ 0.4 και 0.9.
+
+    :rtype: list
+    """
     characters = string.ascii_lowercase[:24]
     num_samples = randint(1, len(characters))
     random_characters = sample(characters, num_samples)
@@ -68,33 +90,47 @@ def get_test_set():
     return test_set
 
 
-def auto_measure_lsh(trees, test):
-    for i in range(len(trees)):
+def auto_testing(trees: list, test: list):
+    """
+    Δημιουργεί τα δέντρα και κάνει αναζήτηση σε αυτά. Στο τέλος δημιουργεί ένα txt με τα αποτελέσματα
+    :param list trees: Λίστα με τις συναρτήσεις δημιουργίας των δέντρων. Χρειαζόμαστε το index της.
+    :param list test: Το Test set
+    :return: Nothing
+    :rtype: None
+    """
+    for i in range(len(trees)):  # χρησιμοποιούμε το index που είναι κοινό στους 2 πίνακες
         try:
-            tree = BUILD_FUNCS[i](test[1])
+            tree = BUILD_FUNCS[i](test[1])  # Επειδή το KD Tree χρειάζεται τα awards στην κατασκευή
         except Exception:
-            tree = BUILD_FUNCS[i]()
+            tree = BUILD_FUNCS[i]()  # για τα υπόλοιπα δέντρα
+        # Αποθήκευση του δέντρου και των αποτελεσμάτων του
         TREES.append(tree)
         results = QUERY_FUNCS[i](tree, test[0], test[1], test[2])
         RESULTS.append(results)
+        # Επιπλέον, τυπώνονται στο terminal συνοπτικά αποτελέσματα
         print(tree.__str__())
         print(f'Found {len(results)} results with Surname starting from {test[0][0]} to {test[0][1]}, Awards: {test[1]}'
               f' and DBLP from {test[2][0]} to {test[2][1]}')
         threshold = test[3]
         similar_science = lsh(results, test[3])
-        while len(similar_science) == 0 and (threshold - 0.1) > 0:
+        # Τυπώνονται τα αποτελέσματα του lsh των αποτελεσμάτων
+        # Αν δε βρει τίποτα το μειώνει στο μισό και ξαναδοκιμάζει
+        while len(similar_science) == 0 and (threshold - 0.1) > 0:  # threshold - 0.1 για να αποφευχθεί infinite loop
             print(f'No similar for {threshold * 100:.2f} %')
             threshold = threshold / 2
             similar_science = lsh(results, threshold)
-        # print(similar_science)
+        # print(similar_science)  # DEBUG
         print(f'For similarity above: {threshold * 100:.2f} % the results are: {len(similar_science)}')
         if threshold <= 0:
             print(f'No similar for {test[3] * 100:.2f} %')
         print()
         print((lambda: "-" * 50)())
-    save_experiment(TREES, RESULTS, test)
+    save_experiment(TREES, RESULTS, test)  # Αποθήκευση των αποτελεσμάτων
 
 
 if __name__ == '__main__':
     for i in range(100):
-        auto_measure_lsh(BUILD_FUNCS, get_test_set())
+        # Λίστες για τα αποτελέσματα και τα δέντρα. Οι λίστες θα είναι ευθυγραμμισμένες ώστε να έχουν κοινό index
+        TREES = []
+        RESULTS = []
+        auto_testing(BUILD_FUNCS, get_test_set())
